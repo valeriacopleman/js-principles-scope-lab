34e61f63c1d8d94ed84685549e04e6f4b4ea47f1
Valeria Copleman <valeriacopleman@gmail.com>
2021-02-24 00:25:03 -0800

    commit tree 77a5c9ad9b7cd11fbe92da719f48d58cbed14788

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+
+function upperCaseCustomerName() {
+   //return customerName.toUpperCase();
+   customerName = 'BOB';
+   //return customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'bob'
+
+function changeLeastFavoriteCustomer() {
+     leastFavoriteCustomer = 'not bob';
+
+    //return leastFavoriteCustomer;
+}
